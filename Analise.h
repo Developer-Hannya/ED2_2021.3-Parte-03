6dafda6d83067fca44ecb63910f0982816daa25c
Joao Pedro Costa Lopes <68126578+Joaopclopes@users.noreply.github.com>
2022-01-17 11:57:42 -0300

    Create Analise.h

---
+++ Analise.h
@@ -0,0 +1,21 @@
+#ifndef Analise_H_INCLUDED
+#define Analise_H_INCLUDED
+
+#include <iostream>
+#include <vector>
+
+#include "TikTokData.h"
+
+class Analise
+{
+private:
+    
+public:
+    Analise();
+
+    vector<TikTokData> importacao(int N);
+    void insercao(vector<TikTokData> registrosimportados,RedBlackTree VP,BTree AVL);
+    void busca(int B,RedBlackTree VP,BTree AVL,vector<TikTokData> registrosimportados);
+};
+
+#endif
